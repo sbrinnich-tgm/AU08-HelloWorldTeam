1c494617ba3411cec1a59bce7cdfe4ff88c412d7
Niklas Hohenwarter <nhohenwarter@student.tgm.ac.at>
2014-03-07 11:28:03 +0100

    Kaadsee

--- AU08-HelloWorldTeam_sbrinnich/src/helloWorld/HelloWorld.java
+++ AU08-HelloWorldTeam_sbrinnich/src/helloWorld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		selina();
 		inga();
+		niklas();
 	}
 	
 	public static void selina(){
@@ -13,6 +14,10 @@ public class HelloWorld {
 	
 	public static void inga(){
 		System.out.println("Schöne Grüße von Inga!");
+	}
+	
+	public static void niklas(){
+		System.out.println("Grüße von Niklas! :)");
 	}
 
 }
