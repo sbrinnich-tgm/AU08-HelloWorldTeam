a2007d24082bceef83a56c4d89dd703ae3502878
Brinnich Selina <sbrinnich@student.tgm.ac.at>
2014-03-07 10:37:02 +0100

    Klasse erstellt/1. Methode hinzugefügt

---
+++ AU08-HelloWorldTeam_sbrinnich/src/helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		selina();
+	}
+	
+	public static void selina(){
+		System.out.println("Schöne Grüße von Selina!");
+	}
+
+}
