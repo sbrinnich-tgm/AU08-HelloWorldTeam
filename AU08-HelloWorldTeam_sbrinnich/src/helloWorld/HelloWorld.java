bbebc5e28b96415bafeb8abff8ed934d57e652da
Keilholz Inga <ikeilholz@student.tgm.ac.at>
2014-03-07 11:04:31 +0100

    2. Methode hinzugefügt

--- AU08-HelloWorldTeam_sbrinnich/src/helloWorld/HelloWorld.java
+++ AU08-HelloWorldTeam_sbrinnich/src/helloWorld/HelloWorld.java
@@ -4,10 +4,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		selina();
+		inga();
 	}
 	
 	public static void selina(){
 		System.out.println("Schöne Grüße von Selina!");
+	}
+	
+	public static void inga(){
+		Sytstem.out.println("Schöne Grüße von Inga!");
 	}
 
 }
